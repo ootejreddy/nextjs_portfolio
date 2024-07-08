0432093f63a4ecae9373b7c935b488c4bffa7ad5
ootejreddy <mail2bsr007@gmail.com>
2024-07-08 18:57:05 -0500

    Added Toast notification and footer for the web page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
+import { Toaster } from "react-hot-toast";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +29,8 @@ export default function RootLayout({
         <ActiveSectionContextProvider>
           <Header />
           {children}
+          <Footer />
+          <Toaster position="top-right" />
         </ActiveSectionContextProvider>
       </body>
     </html>
